669a062331a0ae975aca41419a3717f29d8a13ce
GalakticX <68572576+GalakticX@users.noreply.github.com>
2023-09-18 09:51:58 -0300

    Create register.tsx
    working on register page

---
+++ src/pages/register.tsx
@@ -0,0 +1,9 @@
+import react, { useState } from "react";
+
+const Register = () => {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  async function handleSubmit() {
+    try {
+    } catch (error) {}
+  }
+};
