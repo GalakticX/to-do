f6024581102d58c9e683349879c122cc79c2a62b
GalakticX <68572576+GalakticX@users.noreply.github.com>
2023-09-18 09:51:52 -0300

    Create login.tsx

---
+++ src/pages/login.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import { useRouter } from "next/router";
+
+const router = useRouter();
+const Login = () => {
+  const [formData, setFormData] = useState({ username: "", password: "" });
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        router.push("/");
+      } else {
+        setError("Invalid Credentials");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <div>
+      {error && <h3>{error}</h3>}
+      <form action="/login" onSubmit={handleSubmit}>
+        <label htmlFor="username">
+          Username
+          <input id="username" type="text" onChange={handleChange} required />
+        </label>
+        <label htmlFor="password">
+          {" "}
+          Password
+          <input type="password" onChange={handleChange} required />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
